3190e61a796896fd5392e8432bcc7306625cf3da
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-14 14:03:19 +0100

    create and apply global styles

--- src/templates/Main/index.js
+++ src/templates/Main/index.js
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import theme from '../../theme/theme';
+import GlobalStyles from '../../theme/GlobalStyles';
 
-const Main = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+const Main = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    {children}
+  </ThemeProvider>
+);
 
 Main.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
