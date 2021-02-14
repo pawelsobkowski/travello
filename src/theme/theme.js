36cf7ce4bdb749e5e013a692aa3cb13e8e93da56
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-14 13:46:10 +0100

    create theme

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    lightgrey: '#FAFAFA',
+    grey: '#CBCBCB',
+    bc_grey: '#F5F5F5',
+    blur: '#121110',
+    primary_blue: '#1EA0E1',
+    lightblue: '#BCE9FF',
+    text_blue: '#0E84BF',
+    profile_blue: '#003B59',
+  },
+};
+
+export default theme;
