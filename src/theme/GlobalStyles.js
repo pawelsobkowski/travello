cd3f3a23120f92f9dccae86524eaeba73a4fee22
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-14 16:05:16 +0100

    add more global styles
    add padding and box-sizing

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
 `;
 
