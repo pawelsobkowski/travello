c92947b4e0cc1127ff65a93290a3199d295ab1de
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-15 21:43:07 +0100

    add in Poppins font to project

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+    :root {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    button, input {
+        font-family: 'Poppins', sans-serif;
+    }
+
     body {
         margin: 0;
         padding: 0;
