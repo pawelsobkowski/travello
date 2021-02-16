0d322205595ad22d6e6a70734802bdade87625ca
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-16 20:21:25 +0100

    set border-box to all elements

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -11,10 +11,13 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
     }
 
+    * {
+        box-sizing: border-box;
+    }
+
     body {
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
     }
 `;
 
