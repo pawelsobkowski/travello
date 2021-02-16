f59a23150a681361ca366e895714fd37933ca759
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-16 22:14:38 +0100

    add signin button when user is logged out

--- src/templates/Header/styles.js
+++ src/templates/Header/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import hexToRgba from 'hex-to-rgba';
+import { Link } from 'react-router-dom';
 
 const Header = styled.header`
   display: grid;
-  grid-template-columns: 1fr 100px;
+  grid-template-columns: 1fr 150px;
   position: fixed;
   top: 0;
   left: 0;
@@ -27,9 +29,28 @@ const ButtonsWrapper = styled.div`
   grid-gap: 10px;
 `;
 
+const SignInButton = styled(Link)`
+  height: 40px;
+  width: 100px;
+  border-radius: 10px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 1.7rem;
+  text-decoration: none;
+  font-weight: 500;
+  background-color: ${({ theme }) => hexToRgba(theme.colors.grey, '0.9')};
+  color: ${({ theme }) => theme.colors.white};
+
+  /* & > a {
+    width: 100%;
+    height: 100%;
+    } */
+`;
+
 const Styled = {
   Header,
   ButtonsWrapper,
+  SignInButton,
 };
 
 export default Styled;
