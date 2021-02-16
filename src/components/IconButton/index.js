7650da477c699ee47bd00aff5340476b447bb171
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-16 21:51:40 +0100

    create icon button component

---
+++ src/components/IconButton/index.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import Styled from './styles';
+import { ReactComponent as Avatar } from '../../assets/icon/avatar.svg';
+
+const IconButton = ({ icon, avatar }) => (
+  <Styled.Button type="button">
+    {avatar ? <Styled.AvatarBox avatar={avatar} alt="" /> : icon}
+  </Styled.Button>
+);
+
+IconButton.propTypes = {
+  icon: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  avatar: PropTypes.string,
+};
+
+IconButton.defaultProps = {
+  icon: <Avatar />,
+  avatar: '',
+};
+
+export default IconButton;
---
+++ src/components/IconButton/styles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import hexToRgba from 'hex-to-rgba';
+
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 0;
+  cursor: pointer;
+  background-color: ${({ theme }) => hexToRgba(theme.colors.grey, '0.7')};
+  color: ${({ theme }) => hexToRgba(theme.colors.white)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+
+  & > svg {
+    width: 60%;
+    height: 60%;
+  }
+
+  & > svg > * {
+    stroke: ${({ theme }) => theme.colors.white};
+    fill: none;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+const AvatarBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background: url(${({ avatar }) => avatar}) center no-repeat;
+  background-size: cover;
+`;
+
+const Styled = {
+  Button,
+  AvatarBox,
+};
+
+export default Styled;
