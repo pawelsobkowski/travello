87ce6fd15def6bce2f9fa1893460f370c186b518
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-20 20:52:31 +0100

    create text animation at home page

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -1,26 +1,127 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Main = styled.main`
+  height: 100vh;
+  padding-top: 60px;
+  overflow: hidden;
+  background: url(${({ background }) => background}) center no-repeat;
+  background-size: cover;
+  color: ${({ theme }) => theme.colors.white};
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    padding-top: 70px;
+  }
+`;
 
 const Wrapper = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  padding: 20px;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    padding: 5px;
+    grid-template-columns: 10% 90%;
+  }
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-template-rows: 50% 50%;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.laptop}) {
+    grid-template-rows: 100%;
+    grid-template-columns: 55% 45%;
+  }
+`;
+
+const TextWrapper = styled(motion.section)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TitleWrapper = styled(motion.div)`
+  overflow: hidden;
+  position: relative;
+`;
+
+const Title = styled(motion.h1)`
+  font-size: 4rem;
+  text-transform: uppercase;
+  line-height: 100%;
+  position: absolute;
+  top: 100%;
+  left: 0;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    font-size: 6rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.desktop}) {
+    font-size: 7rem;
+  }
+`;
+
+const DescriptionWrapper = styled(motion.div)`
+  position: relative;
+  overflow: hidden;
+`;
+
+const Description = styled(motion.p)`
+  font-size: 1.3rem;
+  position: absolute;
+  top: 100%;
+  left: 0;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.desktop}) {
+    font-size: 1.7rem;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
   width: 100%;
-  max-width: 400px;
+  max-width: 350px;
   height: 45px;
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 15px;
   grid-template-columns: 1fr 1fr;
 
   @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
-    max-width: 500px;
+    max-width: 450px;
     height: 55px;
   }
 
   @media (min-width: ${({ theme }) => theme.breakingPoints.laptop}) {
-    max-width: 600px;
+    max-width: 500px;
     height: 70px;
   }
 `;
 
+const DotsWrapper = styled.div`
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    display: block;
+  }
+`;
+
 const Styled = {
+  Main,
   Wrapper,
+  Content,
+  ButtonsWrapper,
+  TextWrapper,
+  TitleWrapper,
+  Title,
+  DescriptionWrapper,
+  Description,
+  DotsWrapper,
 };
 
 export default Styled;
