2eddf11461f0019d258eabe41b34b39909f609bd
Pawel Sobkowski <psobkowski98@gmail.com>
2021-02-21 23:51:01 +0100

    Add styles to get 100% viewport

--- src/theme/GlobalStyles.js
+++ src/theme/GlobalStyles.js
@@ -11,13 +11,20 @@ const GlobalStyles = createGlobalStyle`
         padding: 0;
     }
 
+    html, body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
     * {
         box-sizing: border-box;
     }
 
-    body {
-        margin: 0;
-        padding: 0;
+    #root{
+        width: 100%;
+        height: 100%;
     }
 `;
 
