3424db6a7fa7b7fea87c1ad909e463292a90e6e2
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-24 20:22:05 +0100

    create slider context

---
+++ src/context/sliderContext.js
@@ -0,0 +1,40 @@
+import { createContext, useReducer, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const SliderContext = createContext();
+
+const sliderReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_PROPERTIES': {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+};
+
+export const SliderProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(sliderReducer, {
+    current: 0,
+    order: [0, 1, 2, 3],
+    direction: 1,
+  });
+
+  return (
+    <SliderContext.Provider value={{ sliderStatus: state, dispatch }}>
+      {children}
+    </SliderContext.Provider>
+  );
+};
+
+SliderProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+};
+
+export const useSliderStateContext = () => useContext(SliderContext).sliderStatus;
+
+export const useSliderDispatchContext = () => useContext(SliderContext).dispatch;
