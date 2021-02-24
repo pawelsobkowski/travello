66ec47281bdb11d8e6f199c363876029091d2dfd
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-24 20:22:22 +0100

    apply slider context in main template

--- src/templates/Main/index.js
+++ src/templates/Main/index.js
@@ -2,11 +2,12 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import theme from '../../theme/theme';
 import GlobalStyles from '../../theme/GlobalStyles';
+import { SliderProvider } from '../../context/sliderContext';
 
 const Main = ({ children }) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    {children}
+    <SliderProvider>{children}</SliderProvider>
   </ThemeProvider>
 );
 
