6fab4fbd62a25aaf939fc89c7feaae5b6698f289
Sebastian Zawolanski <sebastol90@wp.pl>
2021-02-25 18:54:04 +0100

    create variant that changes slider cards

--- src/variants/home.js
+++ src/variants/home.js
@@ -1,10 +1,14 @@
+const initialConfig = {
+  transition: {
+    ease: 'easeInOut',
+    duration: 0.5,
+  },
+};
+
 const currentPlaceText = (height, direction) => ({
   visible: {
     y: height * -1,
-    transition: {
-      ease: 'easeInOut',
-      duration: 0.5,
-    },
+    ...initialConfig,
   },
   hidden: { y: direction === -1 ? 0 : height * -2 },
 });
@@ -12,17 +16,120 @@ const currentPlaceText = (height, direction) => ({
 const previousPlaceText = (height, direction) => ({
   visible: {
     y: direction === -1 ? height * 2 * direction : 0,
-    transition: {
-      ease: 'easeInOut',
-      duration: 0.5,
-    },
+    ...initialConfig,
   },
   hidden: { y: height * -1 },
 });
 
+const nextSliderCard = (previousPlace, direction, currentCard, width = 166) => {
+  if (previousPlace !== -1) {
+    // direction: left
+    if (direction === -1) {
+      switch (currentCard) {
+        // first card
+        case 0:
+          return {
+            visible: {
+              x: width * direction,
+              opacity: 0,
+              scale: 0.8,
+              transitionEnd: {
+                x: width * 4,
+              },
+              ...initialConfig,
+            },
+          };
+        // third card
+        case 2:
+          return {
+            visible: {
+              x: width,
+              ...initialConfig,
+            },
+          };
+        // last card
+        case 3:
+          return {
+            visible: {
+              x: width * 2,
+              ...initialConfig,
+            },
+          };
+        // second card
+        default:
+          return {
+            visible: {
+              x: 0,
+              ...initialConfig,
+            },
+          };
+      }
+    }
+    // direction: right
+    else {
+      switch (currentCard) {
+        // last card
+        case 0:
+          return {
+            visible: {
+              x: width * 3,
+              opacity: 0,
+              scale: 0.8,
+              ...initialConfig,
+            },
+          };
+        // second card
+        case 2:
+          return {
+            visible: {
+              x: width,
+              ...initialConfig,
+            },
+          };
+
+        // third card
+        case 3:
+          return {
+            visible: {
+              x: width * 2,
+
+              ...initialConfig,
+            },
+          };
+        // first card
+        default:
+          return {
+            visible: {
+              x: 0,
+              zIndex: 0,
+              transitionEnd: {
+                zIndex: 2,
+              },
+              ...initialConfig,
+            },
+          };
+      }
+    }
+  }
+  // initial
+  if (currentCard === 0) {
+    return {
+      visible: { x: width * 4, opacity: 0, transition: { duration: 0 } },
+    };
+  }
+  if (currentCard === 1) {
+    return { visible: { x: 0, transition: { duration: 0 } } };
+  }
+  if (currentCard === 2) {
+    return { visible: { x: width, transition: { duration: 0 } } };
+  }
+  return { visible: { x: width * 2, transition: { duration: 0 } } };
+};
+
 const Variants = {
   currentPlaceText,
   previousPlaceText,
+  nextSliderCard,
 };
 
 export default Variants;
