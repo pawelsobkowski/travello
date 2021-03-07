c4353e5805fe64f0fcb88fcb4ed1f8b913d9d746
Sebastian Zawolanski <sebastian.zawolanski@gmail.com>
2021-03-07 17:55:30 +0100

    apply form, title, social button and form message

--- src/views/Signin/index.js
+++ src/views/Signin/index.js
@@ -1,5 +1,20 @@
 import Styled from './styles';
+import SignInForm from '../../forms/signin';
+import Title from '../../components/SectionTitle';
+import Button from '../../components/FormButton';
+import FormMessage from '../../components/FormMessage';
 
-const Signin = () => <Styled.Wrapper>Sign in</Styled.Wrapper>;
+const Signin = () => (
+  <Styled.FormContainer>
+    <Title>Sign in</Title>
+    <Styled.Description>Log in and explore the world.</Styled.Description>
+    <Button isSocial>Sign in with Google</Button>
+    <Styled.Text>or sign in with email</Styled.Text>
+    <SignInForm />
+    <FormMessage to="/home/signup" linkLabel="Sign up">
+      You don&apos;t have an account?
+    </FormMessage>
+  </Styled.FormContainer>
+);
 
 export default Signin;
--- src/views/Signin/styles.js
+++ src/views/Signin/styles.js
@@ -1,13 +1,47 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  width: 400px;
-  background-color: green;
+const FormContainer = styled.div`
+  width: 100%;
+  max-width: 30rem;
   margin: auto;
+  padding: 1.2rem;
+`;
+
+const Description = styled.p`
+  color: ${({ theme }) => theme.colors.white_063};
+  font-size: 1.3rem;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.desktop}) {
+    font-size: 1.7rem;
+  }
+`;
+
+const Text = styled.p`
+  color: ${({ theme }) => theme.colors.white_063};
+  font-size: 1rem;
+  text-align: center;
+  margin: 2.5rem 0;
+  font-weight: bold;
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.tablet}) {
+    font-size: 1.2rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakingPoints.desktop}) {
+    font-size: 1.4rem;
+  }
 `;
 
 const Styled = {
-  Wrapper,
+  Text,
+  FormContainer,
+  Description,
 };
 
 export default Styled;
