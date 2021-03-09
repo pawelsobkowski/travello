f2d57f969031f274e39e747ff05b3832699ff32e
Sebastian Zawolanski <sebastian.zawolanski@gmail.com>
2021-03-09 13:05:46 +0100

    create show password functionality

--- src/components/Controls/input.js
+++ src/components/Controls/input.js
@@ -1,11 +1,27 @@
 import PropTypes from 'prop-types';
 import { ErrorMessage } from 'formik';
 import Styled from './styles';
+import { ReactComponent as Eye } from '../../assets/icon/eye.svg';
+import { ReactComponent as EyeOff } from '../../assets/icon/eye-off.svg';
+import IconButton from '../IconButton';
 
-const Input = ({ label, name, ...rest }) => (
+const Input = ({ label, name, isPassword, type, changeType, ...rest }) => (
   <>
     <Styled.Label htmlFor={name}>{label}</Styled.Label>
-    <Styled.Input placeholder={label} name={name} id={name} {...rest} />
+    <Styled.InputWrapper>
+      <Styled.Input placeholder={label} name={name} id={name} type={type} {...rest} />
+      {isPassword && (
+        <Styled.ShowPasswordWrapper>
+          <IconButton
+            aria-controls={name}
+            aria-expanded={type !== 'password'}
+            type="button"
+            onClick={changeType}
+            icon={type === 'password' ? <Eye /> : <EyeOff />}
+          />
+        </Styled.ShowPasswordWrapper>
+      )}
+    </Styled.InputWrapper>
     <Styled.ErrorMessage>
       <ErrorMessage name={name} />
     </Styled.ErrorMessage>
@@ -15,6 +31,14 @@ const Input = ({ label, name, ...rest }) => (
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  isPassword: PropTypes.bool,
+  changeType: PropTypes.func,
+};
+
+Input.defaultProps = {
+  isPassword: false,
+  changeType: () => {},
 };
 
 export default Input;
